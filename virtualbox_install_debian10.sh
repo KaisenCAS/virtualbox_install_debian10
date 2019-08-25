6411e914b87282275251d0f7bef9353f3d3595e4
Kaisen <kevinchevreuillinux@gmail.com>
2019-08-25 22:29:17 +0200

    Update virtualbox_install_debian10.sh

--- virtualbox_install_debian10.sh
+++ virtualbox_install_debian10.sh
@@ -3,7 +3,7 @@
 echo 'This script install Virtual 6.0 for Debian 10 users.' 
 
 #Add repository for install Virtualbox 6.0 
-echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee -a >> sudo /etc/apt/sources.list 
+echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee -a /etc/apt/sources.list 
 #Move to /tmp for download GPG signed key
 cd /tmp && sudo wget https://www.virtualbox.org/download/oracle_vbox_2016.asc
 #Install GPG signed key for VirtualBox repository
