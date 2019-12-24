4d3bc6c3461269f9fd7ad87a04afccdd739df3c6
Kaisen <kaisen@localhost.localdomain>
2019-12-24 01:29:41 +0100

    Add checking root execution

--- virtualbox_install_debian10.sh
+++ virtualbox_install_debian10.sh
@@ -1,12 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-echo 'This script install Virtual 6.1 for Debian 10 users.' 
+#Check root execution verification
+if [[ "$EUID" -ne 0 ]]; then
+	echo -e "Run this script as root"
+	exit 1
+fi
 
-#Add repository for install Virtualbox 6.1 
-echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
+echo 'This script install Virtual 6.1 for Debian 10 users.'
+
+#Add repository for install Virtualbox 6.1
+echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian bionic contrib" | tee -a /etc/apt/sources.list.d/virtualbox.list
 #Move to /tmp for download GPG signed key
-cd /tmp && sudo wget https://www.virtualbox.org/download/oracle_vbox_2016.asc
+cd /tmp && wget https://www.virtualbox.org/download/oracle_vbox_2016.asc
 #Install GPG signed key for VirtualBox repository
-sudo apt-key add oracle_vbox_2016.asc
+apt-key add oracle_vbox_2016.asc
 #Update repositories and Virtualbox install
-sudo apt-get update && sudo apt-get install virtualbox-6.1 -y 
+apt-get update && sudo apt-get install virtualbox-6.1 -y
